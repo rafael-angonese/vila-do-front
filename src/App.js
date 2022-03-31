a27d6f615f1763712afa4abe7061b1cd04f869be
Rafael Angonese <rafael.angonese09@gmail.com>
2022-03-31 20:50:10 -0300

    :sparkles: chore: use component with props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import Button from "./components/Button";
-import Clients from "./components/Clients"
+import Clients from "./components/Clients";
 import ConctactForm from "./components/ContactForm";
+import Label from "./components/Label";
 
 function App() {
   return (
-    <div >
+    <div>
       <h1>Vila do Front</h1>
-      <Button />
+      <Button >Comprar</Button>
 
-      <Clients />
+      <Button color="red" size="20px" >Vender</Button>
+
+      <Button color="blue">Historico</Button>
+
+      <br />
+      <Label>Nome</Label>
+      <Label>Resilia</Label>
+  
+
+      {/* <Clients /> */}
 
       <ConctactForm />
-      
     </div>
   );
 }
