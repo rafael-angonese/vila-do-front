3d56184c8c2c9abaf533d9a2145009a4d5da6b05
Rafael Angonese <rafael.angonese09@gmail.com>
2022-03-30 21:11:22 -0300

    :sparkles: chore: use components into App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import Button from "./components/Button";
+import Clients from "./components/Clients"
+import ConctactForm from "./components/ContactForm";
+
 function App() {
   return (
     <div >
       <h1>Vila do Front</h1>
+      <Button />
+
+      <Clients />
+
+      <ConctactForm />
+      
     </div>
   );
 }
