70c4745872b52d90c999b5aa9b5810f2fccb5c72
Rafael Angonese <rafael.angonese09@gmail.com>
2022-04-04 20:00:59 -0300

    :sparkles: chore: included Header into App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import Button from "./components/Button";
 import Clients from "./components/Clients";
 import ConctactForm from "./components/ContactForm";
+import Header from "./components/Header/Header";
 import Label from "./components/Label";
 
 function App() {
   return (
-    <div>
+    <>
+      <Header />
       <h1>Vila do Front</h1>
-      <Button >Comprar</Button>
+      <Button>Comprar</Button>
 
-      <Button color="red" size="20px" >Vender</Button>
+      <Button color="red" size="20px">
+        Vender
+      </Button>
 
       <Button color="blue">Historico</Button>
 
       <br />
       <Label>Nome</Label>
       <Label>Resilia</Label>
-  
 
-      {/* <Clients /> */}
+      <Clients />
 
       <ConctactForm />
-    </div>
+    </>
   );
 }
 
