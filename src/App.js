819ae292ccb8e9171c6f18277b97b4750181096f
Rafael Angonese <rafael.angonese09@gmail.com>
2022-04-04 21:05:01 -0300

    :sparkles: feat: use svgs into react

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import ConctactForm from "./components/ContactForm";
 import Header from "./components/Header/Header";
 import Label from "./components/Label";
 
-import about from './assets/about-pic.jpg'
+import about from "./assets/about-pic.jpg";
+
+import { ReactComponent as Wave } from "./assets/imagens/wave.svg";
+import { ReactComponent as Truque } from "./assets/imagens/truque.svg";
+import WaveSvg from "./components/WaveSvg";
 
 function App() {
   return (
@@ -19,8 +23,23 @@ function App() {
 
       <Button color="blue">Historico</Button>
 
-      <img alt="images" src={require('./assets/about-pic.jpg')} />
-      <img  src={about} />
+      <div style={{ width: "100px" }}>
+        <Truque />
+      </div>
+
+      <div style={{ width: "300px" }}>
+        <Wave />
+      </div>
+
+      <div style={{ width: "300px" }}>
+        <WaveSvg />
+      </div>
+
+      <br />
+      <br />
+
+      <img alt="images" src={require("./assets/about-pic.jpg")} />
+      <img src={about} />
 
       <br />
       <Label>Nome</Label>
