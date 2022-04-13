d1e760e117ed5722ff3c54d293cd9df87c3164c9
Rafael Angonese <rafael.angonese09@gmail.com>
2022-04-13 20:53:17 -0300

    :recycle: refactor: FairyCard to use fairy prop

--- src/components/FairyCard/FairyCard.jsx
+++ src/components/FairyCard/FairyCard.jsx
@@ -1,23 +1,25 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../Button/Button";
 import styles from "./styles.module.css";
 
 
 const FairyCard = ({ fairy, onDelete }) => {
+  const navigate = useNavigate()
 
   return (
     <div className={styles.card}>
       <h1>
-        Name: <span>{fairy.name}</span>
+        Name: <span>{fairy?.name}</span>
       </h1>
 
       <p>
-        Elemento: <span>{fairy.element}</span>
+        Elemento: <span>{fairy?.element}</span>
       </p>
       <p>
-        Vida: <span>{fairy.health_point}</span>
+        Vida: <span>{fairy?.health_point}</span>
       </p>
 
-      <Button>Visualizar</Button>
+      <Button onClick={() => navigate(`/fairies/${fairy.id}`)}>Visualizar</Button>
       <Button onClick={(event) => onDelete(fairy.id)}>Excluir</Button>
       <Button>Editar</Button>
     </div>
