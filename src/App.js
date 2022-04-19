2ecc217d19d7dd150ce77fc6790ae6c6f9885ab9
Rafael Angonese <rafael.angonese09@gmail.com>
2022-04-18 21:30:03 -0300

    :sparkles: feat: use styled components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,36 @@ import { ToastContainer } from "react-toastify";
 import { BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 
+import styled from "styled-components";
+
 import Header from "./components/Header/Header";
 import Rotas from "./routes";
+import Title from "./components/Title/Title";
 
 function App() {
+  const Button = styled.button`
+    background: ${(props) => (props.primary ? "palevioletred" : "white")};
+    color: ${(props) => (props.primary ? "white" : "palevioletred")};
+
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid palevioletred;
+    border-radius: 3px;
+  `;
+
   return (
     <>
       <BrowserRouter>
         <Header />
 
-        <Rotas />
+        <Button>Normal</Button>
+        <Button primary>Primary</Button>
 
+        <Title color="blue" size={50} >Nosso Titulo em Styled Components</Title>
+
+
+        <Rotas />
       </BrowserRouter>
       <ToastContainer />
     </>
