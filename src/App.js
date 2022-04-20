f68366c1e56f31632473a5d61acf5b63be3cbd07
Rafael Angonese <rafael.angonese09@gmail.com>
2022-04-19 21:23:09 -0300

    :sparkles: feat: configure and use react-boostrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import { ToastContainer } from "react-toastify";
 import { BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import Button from 'react-bootstrap/Button';
 
 import Header from "./components/Header/Header";
 import Rotas from "./routes";
+import { Badge } from "react-bootstrap";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Header />
+
+        <Button>Teste React boostrap </Button>
+        <Button variant="primary">Primary</Button>{' '}
+
+        <Badge bg="primary">Primary</Badge> <Badge bg="danger">Danger</Badge>
 
         <Rotas />
 
